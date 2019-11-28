258ed3795f0c99c1ad947b8f0c94f9055ed67fa3
Jakob Kastelic <kastelic.jakob@gmail.com>
2019-11-27 19:15:38 -0500

    cleanup

--- slurm/dsq-jobs-2019-11-27.sh
+++ slurm/dsq-jobs-2019-11-27.sh
@@ -5,5 +5,5 @@
 #SBATCH --nodes=1 --ntasks=3 --cpus-per-task=8 --time=24:00:00
 
 # DO NOT EDIT LINE BELOW
-/gpfs/loomis/apps/avx/software/dSQ/0.96/dSQBatch.py /gpfs/loomis/project/demille/jk2534/State-evolution/jobs.txt /gpfs/loomis/project/demille/jk2534/State-evolution
+/gpfs/loomis/apps/avx/software/dSQ/0.96/dSQBatch.py /gpfs/loomis/project/demille/jk2534/State-evolution/slurm/jobs.txt /gpfs/loomis/project/demille/jk2534/State-evolution/slurm
 
