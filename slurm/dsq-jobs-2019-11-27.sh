995069b11a4463dda56e20453b41e90ab82eb824
Jakob Kastelic <kastelic.jakob@gmail.com>
2019-11-27 21:06:25 -0500

    more calculation

--- slurm/dsq-jobs-2019-11-27.sh
+++ slurm/dsq-jobs-2019-11-27.sh
@@ -2,7 +2,7 @@
 #SBATCH --output dsq-jobs-%A_%1a-%N.out
 #SBATCH --array 0-7
 #SBATCH --job-name dsq-jobs
-#SBATCH --nodes=1 --ntasks=3 --cpus-per-task=8 --time=24:00:00
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=24 --mem-per-cpu=250M --time=24:00:00 --mail-type=ALL --mail-user=jakob.kastelic@yale.edu
 
 # DO NOT EDIT LINE BELOW
 /gpfs/loomis/apps/avx/software/dSQ/0.96/dSQBatch.py /gpfs/loomis/project/demille/jk2534/State-evolution/slurm/jobs.txt /gpfs/loomis/project/demille/jk2534/State-evolution/slurm
