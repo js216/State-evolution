418a1ab5f39503b274637c493fa144cff075a9d6
Jakob Kastelic <kastelic.jakob@gmail.com>
2019-11-27 19:32:42 -0500

    get some results

--- scan.py
+++ scan.py
@@ -438,4 +438,5 @@ if __name__ == '__main__':
 
     # write results to file
     results_fname = hashlib.md5(open(sys.argv[1],'rb').read()).hexdigest() + ".txt"
-    np.savetxt("results/"+results_fname, results)
+    results_dir = sys.argv[2]
+    np.savetxt(results_dir+results_fname, results)
